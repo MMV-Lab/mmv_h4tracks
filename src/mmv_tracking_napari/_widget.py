1036e9c46e0bca4f3a7c39fb689618983713dd89
Lennart Kowitz <Lennart.Kowitz@isas.de>
2022-04-29 13:30:24 +0200

    Next free label implemented another attempt at fixing tests

--- src/mmv_tracking_napari/_widget.py
+++ src/mmv_tracking_napari/_widget.py
@@ -15,7 +15,7 @@ class MMVTracking(QWidget):
         # Labels
         title = QLabel("<font color='green'>WIP title!</font>")
         next_free = QLabel("Next free label:")
-        next_free_id = QLabel("next_free_id")
+        self.next_free_id = QLabel("next_free_id")
         trajectory = QLabel("Select ID for trajectory:")
         tail = QLabel("Set tail length:")
         load = QLabel("Load .zarr file:")
@@ -99,7 +99,7 @@ class MMVTracking(QWidget):
         help_false_negative = QWidget()
         help_false_negative.setLayout(QHBoxLayout())
         help_false_negative.layout().addWidget(next_free)
-        help_false_negative.layout().addWidget(next_free_id)
+        help_false_negative.layout().addWidget(self.next_free_id)
         help_false_merge = QWidget()
         help_false_merge.setLayout(QHBoxLayout())
         help_false_merge.layout().addWidget(false_merge)
@@ -164,6 +164,7 @@ class MMVTracking(QWidget):
         self.viewer.add_image(self.z1['raw_data/Image 1'][:], name = 'Raw Image')
         self.viewer.add_labels(self.z1['segmentation_data/Image 1'][:], name = 'Segmentation Data')
         self.viewer.add_tracks(self.z1['tracking_data/Image 1'][:], name = 'Tracks', tail_length = self.s_tail.value()) # Use graph argument for inheritance (https://napari.org/howtos/layers/tracks.html)
+        self._get_next_free_id()
 
     def _get_current_slice(self):
         #napari.viewer.current_viewer().dims.set_current_step(0,5)
@@ -175,6 +176,7 @@ class MMVTracking(QWidget):
     def _update_track_tails(self):
         self.viewer.layers.remove('Tracks')
         self.viewer.add_tracks(self.z1['tracking_data/Image 1'][:], name='Tracks', tail_length=self.s_tail.value())
+        self._get_next_free_id()
 
     def _save_zarr(self):
         zarr.save(self.file, self.z1)
@@ -204,6 +206,7 @@ class MMVTracking(QWidget):
         id = int(self.le_trajectory.text())
         if id < 0:
             self.viewer.add_tracks(self.z1['tracking_data/Image 1'][:], name='Tracks', tail_length=self.s_tail.value())
+            self._get_next_free_id()
         else:
             tracks_data = [
                 track
@@ -214,3 +217,13 @@ class MMVTracking(QWidget):
                 print("No tracking data found for id " + str(id))
                 return
             self.viewer.add_tracks(tracks_data, name='Tracks', tail_length=self.s_tail.value())
+            self._get_next_free_id()
+
+    def _get_next_free_id(self):
+        i = 0
+        while True:
+            for element in self.z1['tracking_data/Image 1'][:,0]:
+                if i == element:
+                    i = i + 1
+            break
+        self.next_free_id.setText(str(i))
