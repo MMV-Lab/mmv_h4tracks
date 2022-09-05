a7eeea543cbb1449ab9b051c7ee7b398be52d767
Sonneck <justin.sonneck@isas.de>
2022-09-05 17:53:11 +0200

    euclidean distance metric as scatter plot (needs to be changed)

--- src/mmv_tracking_napari/_widget.py
+++ src/mmv_tracking_napari/_widget.py
@@ -33,11 +33,13 @@ class MMVTracking(QWidget):
         self.speed = [] # example
         self.size = []
         self.direction = []
+        self.euclidean_distance = [] 
         
         # Variables to hold state of layers from when metrics were last run
         self.speed_tracks = [] # example
         self.size_seg = []
         self.direction_tracks = []
+        self.euclidean_distance_tracks = [] 
         
         # Labels
         title = QLabel("<font color='green'>HITL4Trk</font>")
@@ -154,7 +156,7 @@ class MMVTracking(QWidget):
         self.c_plots.addItem("speed")
         self.c_plots.addItem("size")
         self.c_plots.addItem("direction")
-        self.c_plots.addItem("example")
+        self.c_plots.addItem("euclidean distance")  # example
 
         # Line Edits
         self.le_trajectory = QLineEdit("")
@@ -168,6 +170,7 @@ class MMVTracking(QWidget):
         self.ch_speed = QCheckBox("Speed")
         self.ch_size = QCheckBox("Size")
         self.ch_direction = QCheckBox("Direction") # example
+        self.ch_euclidean_distance = QCheckBox("Euclidean distance") 
         
         # Progressbar
         #self.progress = Progress()
@@ -271,6 +274,7 @@ class MMVTracking(QWidget):
         q_eval.layout().addWidget(self.ch_speed)
         q_eval.layout().addWidget(self.ch_size)
         q_eval.layout().addWidget(self.ch_direction) # example
+        q_eval.layout().addWidget(self.ch_euclidean_distance) 
         q_eval.layout().addWidget(btn_export)
 
         # Add zones to self.toolbox
@@ -797,6 +801,19 @@ class MMVTracking(QWidget):
             data = axes.scatter(direction[:,1],direction[:,2],c = np.array([[0,0.240802676,0.70703125,1]]))
             self.window.layout().addWidget(QLabel("Scatterplot: Travel direction & Distance"))
             self.direction_tracks = self.viewer.layers[self.viewer.layers.index("Tracks")].data
+        elif self.c_plots.currentIndex() == 3: # Euclidean distance metric
+            if not (type(self.euclidean_distance) == np.ndarray and np.array_equal(self.viewer.layers[self.viewer.layers.index("Tracks")].data,self.euclidean_distance_tracks)): 
+                self._calculate_euclidean_distance()   
+            euclidean_distance = self.euclidean_distance  
+            unique_ids = np.unique(euclidean_distance[:,0]).astype(int) 
+            axes.set_title("Euclidean distance",{"fontsize": 18,"color": "white"})
+            axes.set_xlabel("x")
+            axes.set_ylabel("y")     
+            data = axes.scatter(euclidean_distance[:,1],euclidean_distance[:,2],c = np.array([[0,0.240802676,0.70703125,1]]))    
+            self.window.layout().addWidget(QLabel("Scatterplot x vs y"))
+            self.euclidean_distance_tracks = self.viewer.layers[self.viewer.layers.index("Tracks")].data   
+            
+                                    
         selector = SelectFromCollection(self, axes, data, unique_ids)
         
         def accept(event):
@@ -1439,6 +1456,21 @@ class MMVTracking(QWidget):
         
         # example 
         ###### METRICS HERE
+    def _calculate_euclidean_distance(self):            # Already implemented in _calculate_travel, we could combine these
+        """
+        Calculates euclidean distance
+        """    
+        for unique_id in np.unique(self.tracks[:,0]):
+            track = np.delete(self.tracks,np.where(self.tracks[:,0] != unique_id),0)
+            x = track[-1,3] - track[0,3]
+            y = track[0,2] - track[-1,2]            
+            euclidean_distance = np.around(np.sqrt(np.square(x) + np.square(y)),2)
+            try:
+                retval = np.append(retval, [[unique_id,euclidean_distance,0]],0)
+            except UnboundLocalError:
+                retval = np.array([[unique_id,euclidean_distance,0]])
+        self.euclidean_distance = retval
+
         
     def _adjust_ids(self):
         """
