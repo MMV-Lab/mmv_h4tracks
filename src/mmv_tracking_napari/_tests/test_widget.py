406471756241d94cc96647e297a1f268651b7773
lennartkowitz <lennart.kowitz@isas.de>
2022-11-30 13:55:22 +0100

    started moving functions to separate file s to reduce clutter added segmentation evaluation added first part of tracking evaluation

--- src/mmv_tracking_napari/_tests/test_widget.py
+++ src/mmv_tracking_napari/_tests/test_widget.py
@@ -1,35 +1,20 @@
-#from mmv_tracking_napari import ExampleQWidget, example_magic_widget
-#import numpy as np
+from mmv_tracking_napari import MMVTracking
+import numpy as np
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    """# make viewer and add an image layer using our fixture
+def test_segmentation_evaluation(make_napari_viewer, capsys):
+    # make viewer and add a label layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
+    viewer.add_labels(np.random.randint(2, size = (1, 100, 100), dtype = int), name = "Segmentation Data")
 
     # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
+    my_widget = MMVTracking(viewer)
 
     # call our widget method
-    my_widget._on_click()
+    my_widget._store_segmentation()
+    my_widget._evaluate_segmentation()
 
     # read captured output and check that it's as we expected
     captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n" """
-    assert True
-    
-def test_example_magic_widget(make_napari_viewer, capsys):
-    """viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n" """
-    assert True
+    assert captured.out == "IoU score for frame 0: 1.0\nDICE score for frame 0: 1.0\nF1 score for frame 0: 1.0\nIoU score for whole movie: 1.0\nDICE score for whole movie: 1.0\nF1 score for whole movie: 1.0\n"
